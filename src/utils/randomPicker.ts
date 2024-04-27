e570252fde3ff1805b38f3dd3db6933808e17f89
kunle001 <adekunle.olanipekun.ko@gmail.com>
2024-04-27 09:03:30 +0100

    utils

--- src/utils/randomPicker.ts
+++ src/utils/randomPicker.ts
@@ -3,42 +3,43 @@ import { Question, QuestionDoc } from "../models/questions";
 import AppError from "./appError";
 
 export function getRandomRange(min: number, max: number, rangeSize: number) {
-    const validMax = max - rangeSize + 1;
-    if (validMax < min) {
-        throw new AppError("Invalid range size", 500);
-    }
-    const start = Math.floor(Math.random() * validMax) + min;
-    return [start, start + rangeSize - 1];
+  if (rangeSize > max - min + 1) {
+    throw new AppError("Invalid range size", 500);
+  }
+
+  const validMax = max - rangeSize + 1;
+  if (validMax < min) {
+    throw new AppError("Invalid range size", 500);
+  }
+
+  const start = Math.floor(Math.random() * validMax) + min;
+  return [start, start + rangeSize - 1];
 }
 
-export async function selectRandomData(type:"question"|"advert") {
-    
-    switch (type){
-        case "question":
-            const selectedQuestions: QuestionDoc[] = [];
-            const questions= await Question.find()
+export async function selectRandomData(type: "question" | "advert") {
+  switch (type) {
+    case "question":
+      const selectedQuestions: QuestionDoc[] = [];
+      const questions = await Question.find();
+      const [start, end] = getRandomRange(0, questions.length, 5);
 
-            const ranges = getRandomRange(0, questions.length, 10);
-        
-            for (let i = 0; i < ranges.length; i++) {
-                const range = ranges[i];
-                selectedQuestions.push(questions[range]);
-            }
-        
-            return selectedQuestions;
-        case "advert":
-            const selectedAdvert: AdvertDoc[] = [];
-            const advert= await Advert.find()
+      for (let i = start; i <= end; i++) {
+        // Corrected loop to iterate from start to end
+        selectedQuestions.push(questions[i]);
+      }
 
-            const advert_ranges = getRandomRange(0, advert.length, 2);
-        
-            for (let i = 0; i < advert_ranges.length; i++) {
-                const range = advert_ranges[i];
-                selectedAdvert.push(advert[range]);
-            }
-        
-            return selectedAdvert;
-    }
-    
+      return selectedQuestions;
+    case "advert":
+      const selectedAdvert: AdvertDoc[] = [];
+      const advert = await Advert.find();
+
+      const advert_ranges = getRandomRange(0, advert.length, 2);
+
+      for (let i = 0; i < advert_ranges.length; i++) {
+        const range = advert_ranges[i];
+        selectedAdvert.push(advert[range]);
+      }
+
+      return selectedAdvert;
+  }
 }
-
