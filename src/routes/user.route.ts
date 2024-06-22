78f20b322cd250313e7d09f2ff98e112e50af86b
kunle001 <adekunle.olanipekun.ko@gmail.com>
2024-06-22 14:55:59 +0100

    requiring auth

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,11 +8,10 @@ const router = express.Router();
 const userController = new UserController();
 const paymentController = new PaymentController();
 
-router.route("/add-interest").post(userController.addInterest);
-
 router.route("/list-of-banks").get(userController.listOfBanks);
 
 router.use(requireAuth);
+router.route("/add-interest").post(userController.addInterest);
 router.route("/my-profile").get(userController.myProfile);
 router.route("/update-profile").patch(userController.updateProfile);
 
