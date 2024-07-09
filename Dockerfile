324d11940bb6e3d1e9a9d98dfb7ae225b07207ec
kunle001 <adekunle.olanipekun.ko@gmail.com>
2024-07-09 19:23:56 +0100

    fantastic execution of azure kubernetes setup

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM --platform=linux/amd64 node:alpine
+
+WORKDIR /app
+COPY package.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Build the Docker image
+docker build -t quizme .
+
+# Tag the Docker image
+docker tag quizme quizme.azurecr.io/quizme:v0.0.1
+
+# Push the Docker image to the registry
+docker push quizme.azurecr.io/quizme:v0.0.1
+
+# apply the kubectl 
+kubectl apply -f kubernetes/depl.yaml
+
+# COMMIT TO GIT 
+# git add . && git commit -m "latest stable quizme" && git push myorigin kunledev
+
+echo "Deployment complete"
+# chmod +x deploy.sh
+
