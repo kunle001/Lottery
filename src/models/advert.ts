693e1bc432227fe65113bf073eb52531ec4abcc5
kunle001 <adekunle.olanipekun.ko@gmail.com>
2024-08-24 17:09:23 +0100

    cta

--- src/models/advert.ts
+++ src/models/advert.ts
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
 import { Password } from "../utils/Password";
 
+enum CTAType {
+  ussd = "ussd",
+  webLink = "web link",
+  deepLink = "deep link",
+  phoneCall = "phone call",
+  email = "email",
+  sms = "sms",
+  location = "location",
+  appStore = "app store",
+  playStore = "play store",
+  share = "share",
+}
+
 interface AdvertAttr {
   name: string;
   url: string[];
   redirect_url: string;
+  cta: CTAType;
   buttonText?: string;
 }
 
@@ -13,6 +27,7 @@ export interface AdvertDoc extends mongoose.Document {
   url: string[];
   redirect_url: string;
   buttonText?: string;
+  cta: CTAType;
 }
 
 interface AdvertModel extends mongoose.Model<AdvertDoc> {
@@ -29,6 +44,7 @@ const AdvertSchema = new mongoose.Schema(
       default: 0,
     },
     buttonText: String,
+    cta: String,
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true }
 );
