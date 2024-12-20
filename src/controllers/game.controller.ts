e1cb5f469475f0eb31e4b8a8bcf2f95b258efd60
kunle001 <adekunle.olanipekun.ko@gmail.com>
2024-12-20 15:50:09 +0100

    game chances increased to 5

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -51,8 +51,8 @@ export class GameController {
     const time_left_hours = time_left / (1000 * 60 * 60);
 
     if (
-      (existingPlayer && existingPlayer.no_of_plays >= 3) ||
-      existingPlayer.incremented_chances >= 3
+      (existingPlayer && existingPlayer.no_of_plays >= 5) ||
+      existingPlayer.incremented_chances >= 5
     ) {
       throw new AppError(
         `try again in ${Math.floor(time_left_hours)} ${
